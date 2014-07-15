2960e6583b78377b3868badecf3fbb82244e6f2e
Avi Haiat <thaiat@ipelia.com>
2014-07-16 00:50:27 +0200

    Clean lint.js file

--- app/templates/gulp/tasks/lint.js
+++ app/templates/gulp/tasks/lint.js
@@ -12,29 +12,19 @@ gulp.task('jshint', function() {
         .pipe(jshint({
             lookup: true
         }))
-
-    .pipe(jshint.reporter('jshint-stylish'))
-        .on('end', function() {
-            growly.notify('All files passed', {
-                title: 'PASSED - JsHint',
-                icon: constants.growly.successIcon
-            });
-        })
+        .pipe(jshint.reporter('jshint-stylish'))
         .pipe(jshint.reporter('fail'))
         .on('error', function() {
-
             growly.notify('One or more jshint error', {
                 title: 'FAILED - JsHint',
                 icon: constants.growly.failedIcon
             });
-
         })
         .on('end', function() {
             growly.notify('All files passed', {
                 title: 'PASSED - JsHint',
                 icon: constants.growly.successIcon
             });
-
         });
 });
 
