642f1537cbf7a4cc099ce2e07072154db6f742d3
Avi Haiat <thaiat@ipelia.com>
2014-07-16 08:27:19 +0200

    Add new unit tests

--- test/test-creation.js
+++ test/test-creation.js
@@ -3,34 +3,50 @@
 var path = require('path');
 var helpers = require('yeoman-generator').test;
 
-describe('angular-famous generator', function () {
-  beforeEach(function (done) {
-    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
-      if (err) {
-        return done(err);
-      }
+describe('angular-famous generator', function() {
+    beforeEach(function(done) {
+        helpers.testDirectory(path.join(__dirname, 'temp'), function(err) {
+            if (err) {
+                return done(err);
+            }
 
-      this.app = helpers.createGenerator('angular-famous:app', [
-        '../../app'
-      ]);
-      done();
-    }.bind(this));
-  });
-
-  it('creates expected files', function (done) {
-    var expected = [
-      // add files you expect to exist here.
-      '.jshintrc',
-      '.editorconfig'
-    ];
-
-    helpers.mockPrompt(this.app, {
-      'someOption': true
+            this.app = helpers.createGenerator('angular-famous:app', [
+                '../../app'
+            ]);
+            done();
+        }.bind(this));
     });
-    this.app.options['skip-install'] = true;
-    this.app.run({}, function () {
-      helpers.assertFile(expected);
-      done();
+
+    it('creates expected files', function(done) {
+        var expected = [
+            'package.json',
+            '.jshintrc',
+            'gulpfile.js',
+            '.editorconfig'
+        ];
+
+        helpers.mockPrompt(this.app, {
+            'someOption': true
+        });
+        this.app.options['skip-install'] = true;
+        this.app.run({}, function() {
+            helpers.assertFile(expected);
+            done();
+        });
     });
-  });
+
+    it('package.json and bower.json has expected appname', function(done) {
+        helpers.mockPrompt(this.app, {
+            'appname': 'my App'
+        });
+        this.app.options['skip-install'] = true;
+        this.app.run({}, function() {
+            var pkg = require(path.join(__dirname, 'temp', 'package.json'));
+            console.log(pkg);
+            helpers.assertFileContent('package.json', /"name": "toto"/);
+            helpers.assertFileContent('bower.json', /"name": "toto"/);
+            done();
+        });
+    });
+
 });
--- test/test-load.js
+++ test/test-load.js
@@ -2,9 +2,9 @@
 'use strict';
 var assert = require('assert');
 
-describe('angular-famous generator', function () {
-  it('can be imported without blowing up', function () {
-    var app = require('../app');
-    assert(app !== undefined);
-  });
+describe('angular-famous generator', function() {
+    it('can be imported without blowing up', function() {
+        var app = require('../app');
+        assert(app !== undefined);
+    });
 });
