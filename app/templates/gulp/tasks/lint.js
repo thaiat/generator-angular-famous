38eabc41be7bd2ee689843397f8194174b039f56
Avi Haiat <thaiat@ipelia.com>
2014-07-16 17:33:59 +0200

    Add lint console feedback

--- app/templates/gulp/tasks/lint.js
+++ app/templates/gulp/tasks/lint.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var map = require('map-stream');
 var combine = require('stream-combiner');
+var chalk = require('jshint-stylish/node_modules/chalk');
 var growly = require('growly');
 var jshint = $.jshint;
 var jscs = $.jscs;
@@ -25,6 +26,7 @@ gulp.task('jshint', function() {
         .pipe(jshint.reporter('jshint-stylish'))
         .pipe(jshint.reporter('fail'))
         .on('error', function() {
+            console.log(chalk.red('Jshint failed'));
             growly.notify('One or more jshint error', {
                 title: 'FAILED - JsHint',
                 icon: constants.growly.failedIcon
@@ -33,6 +35,7 @@ gulp.task('jshint', function() {
         .pipe(map(function() {
             if (!hasError && !hasShown) {
                 hasShown = true;
+                console.log(chalk.green('All Jshint files passed'));
                 growly.notify('All files passed', {
                     title: 'PASSED - JsHint',
                     icon: constants.growly.successIcon
@@ -50,6 +53,7 @@ gulp.task('jscs', function() {
     combined.on('error', function(err) {
         hasError = true;
         console.log(err.toString());
+        console.log(chalk.red('Jscs failed'));
         growly.notify('One or more jscs error', {
             title: 'FAILED - Jscs',
             icon: constants.growly.failedIcon
@@ -59,6 +63,7 @@ gulp.task('jscs', function() {
 
     combined.on('end', function() {
         if (!hasError) {
+            console.log(chalk.green('All Jscs files passed'));
             growly.notify('All files passed', {
                 title: 'PASSED - Jscs',
                 icon: constants.growly.successIcon
